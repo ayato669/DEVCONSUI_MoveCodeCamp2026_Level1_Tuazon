49e51ae10d65173e33113669bed142719e14b6a2
ayato669 <tuazonearl62@gmail.com>
2026-03-28 16:36:58 +0800

    “Mod”

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0x431c5ca3b0882e692b52762c91718550350604a3e389799519cb8d866b419694";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
